0227b9b77de3081ba8f153a422f6e8d6b29b31f2
24shivani1996 <24shivani1996@gmail.com>
2025-12-16 20:59:14 +0530

    commit

---
+++ main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class main{
+    public static void main(String args[]){
+        System.out.println("Hi everyone !! I finaly reached here");
+         System.out.println("Milestone Date == " + new Date());
+         System.out.println("Good to go :-)");
+    }
+}
