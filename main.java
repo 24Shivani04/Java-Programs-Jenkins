60e0e01da822f525edece864951f40a07f4173fc
24shivani1996 <24shivani1996@gmail.com>
2025-12-18 12:26:54 +0530

    ok

--- main.java
+++ main.java
@@ -4,5 +4,6 @@ public class main{
         System.out.println("Hi everyone !! I finaly reached here");
          System.out.println("Milestone Date == " + new Date());
          System.out.println("Good to go :-).. Checkkkkk");
+          System.out.println("Complied and run itself");
     }
 }
